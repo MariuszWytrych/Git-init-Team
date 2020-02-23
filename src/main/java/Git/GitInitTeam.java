8b229e710d6e3d7568cde3fe1816eb2ae9d1d02c
Mateusz <chmielinho@gmail.com>
2020-02-23 10:42:56 +0100

    message added "Hi"

--- src/main/java/Git/GitInitTeam.java
+++ src/main/java/Git/GitInitTeam.java
@@ -3,5 +3,6 @@ package Git;
 public class GitInitTeam {
     public static void main(String[] args) {
         System.out.println("Hello Git In team!");
+        System.out.println("Hello i'm Mateusz !!!!");
     }
 }
